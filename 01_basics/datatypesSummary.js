1f5a2b01f2c45f07f99759030736a1f209df86cb
aashish217 <143348454+aashish217@users.noreply.github.com>
2023-10-12 12:54:29 +0000

    Added Stack and Heap memory concept

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -35,3 +35,44 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++
+
+// There are two types of memory
+
+// 1. Stack ==> Uses in primitive datatypes , ==> It gives us a copy of that variable
+
+// 2. Heap ==> Uses in NOn primitive datatypes, ==> It gives us the original reference of that variable.
+
+// Example of stack memory 
+
+let myNameIs = "Aashish";
+
+let mySecondName = myNameIs;
+
+mySecondName = "Ishu";
+
+// console.log(mySecondName);
+// console.log(myNameIs);
+
+// conclusion ==> Value of myNameIs will not change 
+
+// Example of stack memory 
+
+let user1 = {
+
+    name : "Aashish",
+
+    city : "Gwalior"
+
+}
+
+let user2 = user1;
+
+user2.city = "Delhi";
+
+console.log(user1);
+console.log(user2);
+
+// conclusion ==> Value of city in user1 will also change
